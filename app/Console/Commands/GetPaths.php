c4a5007b7faa26c988ce3abb5bbae1a4f6b5b810
mohammed-saleh163 <mohammed.saleh@miswag.com>
2025-03-17 21:47:04 +0300

    feature: create an artisan command to execute the request to minimize the Postman-related cognitive load

---
+++ app/Console/Commands/GetPaths.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class GetPaths extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api:paths {source} {destination}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List all the possible travel paths';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $response = Http::post('http://127.0.0.1:8000/api/paths', [
+            'source' => $this->argument('source'),
+            'destination' => $this->argument('destination')
+        ]);
+
+        $this->table(['Route', 'Cost'], $response->json());
+    }
+}
