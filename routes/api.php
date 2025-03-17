d9710f33563eff7451350168918b81ee92624cca
mohammed-saleh163 <mohammed.saleh@miswag.com>
2025-03-17 21:46:22 +0300

    delete default routes, no one likes those

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Http\Controllers\PathController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/paths', [PathController::class, 'getPaths']);
