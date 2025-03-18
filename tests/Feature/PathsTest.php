eceed46d9c0efca8112d55c923637e67e3470714
mohammed-saleh163 <mohammed.saleh@miswag.com>
2025-03-18 11:19:18 +0300

    Automate tests for checking the correct number of routes and the uniqueness of the routes

--- tests/Feature/PathsTest.php
+++ tests/Feature/PathsTest.php
@@ -89,3 +89,51 @@ test('Traveling does not excede the destination', function () {
 
     $response->assertStatus(200);
 });
+
+
+test('The api returns the correct number of routes for every path', function () {
+    $source = 'Konoha';
+    $dest = 'Madara'; 
+
+    $response = $this->post('/api/paths', [
+        'source' => $source,
+        'destination' => $dest,
+    ]);
+
+    $cities = config('constants.CITIES'); 
+    $sourceIdx = array_search($source, $cities);
+    $destIdx = array_search($dest, $cities);
+
+    //Two of the elements are always excluded (source and destination are constants in every route)
+    //however, since every item in between can be 'in' or 'out' of the set of locations we travel in each route, 
+    //then it is more like a binary way of counting, hence the '2' in the pow()
+    $diff = $destIdx - $sourceIdx;
+    $expectedNum = pow(2, $diff - 1); // -1 so that source and dest are mutually exclusive
+
+    expect(count($response->json()))->toBe($expectedNum);
+
+    $response->assertStatus(200);  
+});
+
+test('All routes are distinct (no repitition)', function () {
+    $source = 'Tsuchi'; 
+    $dest = 'Madara'; 
+
+    $response = $this->post('/api/paths', [
+        'source' => $source,
+        'destination' => $dest
+    ]);
+
+    //this will return an array of the routes only
+    $routes = array_map(function($item){
+        return $item['route'];
+    }, $response->json());
+
+    $uniqueRoutes = array_unique($routes, 0); //remove duplicates
+
+    //compare counts. if equal, then there were no duplicate routes in the original array
+    expect(count($routes))->toBe(count($uniqueRoutes));
+
+    $response->assertStatus(200);
+});
+
